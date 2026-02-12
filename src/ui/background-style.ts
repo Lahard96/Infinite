616c8bb3cd979d1fe922d36a0cf900a87994624d
Lahard96 <lashondra.hardesty@icloud.com>
2026-02-12 02:01:33 -0600

    Create background-style.ts

---
+++ src/ui/background-style.ts
@@ -0,0 +1,9 @@
+import bgImage from "@/assets/infinite-network-bg.png";
+
+export const backgroundStyle = {
+  backgroundImage: `url(${bgImage})`,
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+  backgroundRepeat: "no-repeat",
+  backgroundAttachment: "fixed",
+};
